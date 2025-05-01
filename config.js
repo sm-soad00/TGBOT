b26de7a431997240233db89731b86b764aefa4a3
SMSOAD <smsoad85@gmail.com>
2025-05-01 15:29:02 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//SOAD
-  ownerNumber: "+8801615298449",//+8801816004207
-  admin: ["5920596290", "8179590682"], // your id
+  ownerUsernames: ["SOAD"],//SOAD
+  ownerNumber: "+8801816004207",//+8801816004207
+  admin: ["7617271430", ""], // 7617271430
   prefix: "/", // dont change prefix 
-  telegramBotToken: "token",//Your Bot Tokens
+  telegramBotToken: "7617271430:AAEAufwZsLOqmyYZDpnXB-fEv7gYkPbcr5g",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
   po0rt: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/smsoad",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
